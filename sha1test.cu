19798c2760be02f671f399d2e49f5b07ed0de43d
ChengbinKing <37439374+ChengbinKing@users.noreply.github.com>
2018-05-11 15:36:51 +0800

    Update sha1test.cu

--- sha1test.cu
+++ sha1test.cu
@@ -6,65 +6,17 @@
 #include<string>
 #include"cuda_runtime.h"
 #include"malloc.h"
-#include"sha.h"
+#include<openssl\sha.h>
+#include<openssl\bn.h>
 #pragma comment(lib, "libeay32.lib")
 #pragma comment(lib, "ssleay32.lib")
 #define MAX_THREADS_PER_BLOCK 1
-void in1() {
-	printf("You need to input your data,first!\n");
-	char*a = (char*)malloc(5000*sizeof(char));
-	scanf("%s", a);
-	unsigned char hash[20];
-	clock_t start1, finish1;
-	float costtime;
-	start1 = clock();
-	sha1_cpu((unsigned char*)a, strlen(a), hash);
-	finish1 = clock();
-	costtime = (float)(finish1 - start1)/CLOCKS_PER_SEC;
-	printf("CPU run time %f seconds\n", costtime);
-	printf("CPU execute result:\n");
-	for (int i = 0; i < 20; i++) printf("%02x ",hash[i]);
-	printf("\n");
-	cudaEvent_t start, stop;
-	float costtime4 = 0.0;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaEventRecord(start, 0);
-	sha1_gpu_global1((unsigned char*)a, strlen(a), hash, MAX_THREADS_PER_BLOCK);
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
-	cudaEventElapsedTime(&costtime4, start, stop);
-	printf("GPU need time is %f seconds\n", costtime4 /1000);
-	cudaEventDestroy(start);
-	cudaEventDestroy(stop);
-	printf("GPU execute result:\n");
-	for (int i = 0; i < 20; i++) printf("%02x ", hash[i]);
-	printf("\n");
-	/*clock_t start3, finish3;
-	float costtime3;
-	start3= clock();
-	gpu_sha1((unsigned char*)a, strlen(a), hash);
-	finish3 = clock();
-	costtime3 = (float)(finish3 - start3) / CLOCKS_PER_SEC;
-	printf("CPU run time %f seconds\n", costtime3);
-	printf("GPU2 execute result:\n");
-	for (int i = 0; i < 20; i++) printf("%02x ", hash[i]);
-	printf("\n");*/
-	clock_t start2, finish2;
-	float costtime2;
-	start2 = clock();//unsigned char*
-	SHA1((unsigned char*)a, strlen(a), hash);
-	//SHA1(b,64,hash);
-	finish2 = clock();
-	costtime2 = (float)(finish2 - start2) / CLOCKS_PER_SEC;
-	printf("OpenSSL run time %f seconds\n", costtime2);
-	printf("OpenSSL execute result:\n");
-	for (int i = 0; i < 20; i++) printf("%02x ", hash[i]);
-	printf("\n");
-}
 int main() {
-	printf("You need to input your data,first!\n");
-	int n = 5;
+	
+	int n;
+	printf("Please set the number of inputs\n");
+	scanf("%d", &n);
+	printf("Then you need to input the message\n");
 	int*ilen1 = (int*)malloc(sizeof(int) * n);
 	unsigned char**input = (unsigned char**)malloc(sizeof(unsigned char*) * n);
 	unsigned char**hash = (unsigned char**)malloc(sizeof(unsigned char*)*n);
@@ -74,14 +26,27 @@ int main() {
 		hash[i] = (unsigned char*)malloc(sizeof(unsigned char) * 512);
 		INPUT[i] = (char*)malloc(sizeof(char) * 1000);
 	}
+	BIGNUM*A = BN_new();
+	printf("Random Data is preparing:\n");
 	for (int i = 0; i < n; i++) {
-		scanf("%s", INPUT[i]);
+		//scanf("%s", INPUT[i]);
+		BN_rand(A, 512, 1, 0);
+		INPUT[i]= BN_bn2hex(A);
 		ilen1[i] = strlen(INPUT[i]);
 		input[i] = (unsigned char*)INPUT[i];
+		printf("%s\n", INPUT[i]);
 	}
+	BN_free(A);
+	clock_t start1, finish1;
+	float costtime;
+	start1 = clock();
 	for (int i = 0; i < n; i++) {
-		sha1_cpu(input[i], ilen1[i], hash[i]);
+		SHA1(input[i], ilen1[i], hash[i]);
 	}
+	finish1 = clock();
+	costtime = (float)(finish1 - start1);
+	printf("OpenSSL run time %f ms\n", costtime);
+	printf("OpenSSL results:\n");
 	//multisha1_gpu(input, ilen1, hash,n);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < 20; j++) {
@@ -89,8 +54,19 @@ int main() {
 		}
 		printf("\n");
 	}
+	cudaEvent_t start, stop;
+	float costtime4 = 0.0;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
 	multisha1_gpu(input, ilen1, hash, n);
-
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&costtime4, start, stop);
+	printf("GPU need time is %f ms\n", costtime4);
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+	printf("GPU Result:\n");
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < 20; j++) {
 			printf("%02x", hash[i][j]);
